bd18445ef6473d61c485f4b82e517e1c7057b353
warczar254 <warczar254@gmail.com>
2026-05-02 17:55:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,32 @@ if (process.env.RESEND_API_KEY) {
 }
 
 // ── MongoDB Connection ──
-mongoose.connect(process.env.MONGODB_URI)
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://warczar254:<db_password>@cluster0.yloyhs6.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
     .then(function() { console.log('MongoDB connected'); })
     .catch(function(err) { console.error('MongoDB error:', err.message); process.exit(1); });
 
